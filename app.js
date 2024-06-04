af22a5945690e4e024b4044da4344976c7f61d40
Harsh Ranjan <shriharshranjangupta@gmail.com>
2024-06-05 00:05:07 +0530

    commiting the entire directory in one go

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = express();
 // Setting up the template engine
 app.set('view engine', 'ejs');
 
-// Static files used for creating
-
+// Static files
 app.use(express.static('public'));
 
 // Fire controllers
@@ -16,6 +15,8 @@ const todoController = require('./controllers/todoController');
 todoController(app);
 
 // Listen to port
-app.listen(3000, () => {
-    console.log('Listening to port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Listening to port ${PORT}');
 });
